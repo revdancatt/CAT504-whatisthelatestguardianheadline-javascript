a171d605b94664a7635313a0bde716ec3d3d1789
Rev Dan Catt <revdancatt@gmail.com>
2011-11-22 15:37:20 +0000

    added some style and the textFile script, some styling and reloading the headline once a minute

--- js/control.js
+++ js/control.js
@@ -1,6 +1,8 @@
 control = {
 	
 	latestAPIUrl: '',
+	lastUpdate: 0,
+	loadMoreTmr: null,
 
 	init: function() {
 		
@@ -12,30 +14,32 @@ control = {
 
 		//	Now set an interval timer to do it automatically every
 		//	minute
+		this.loadMoreTmr = setInterval(control.getLatestHeadline, 60 * 1000);
 
 		
 	},
 
-	getLatestHeadline: function() {
-		
+	getLatestHeadline: function () {
+	
+		console.log('here');
 		$.jsonp({
-		    url: 'http://content.guardianapis.com/search?page-size=1&format=json&callback=?',
-		    success: function(json) {
-
-		    	//	If we have a response and an array or results with just one item in it
-		    	//	then we have gotten back a valid result we may want to work with
-		    	if ('response' in json && 'results' in json.response && json.response.results.length == 1) {
-		    		control.parse(json.response.results[0]);
-		    	} else {
-		    		//	Something odd happened but I'm not quite sure what, possibly an over rate message
-		    		//	we'll do something super clever with it here
-		    	}
-		    },
-		    error: function() {
-		    	alert('need to catch this error');
-		    }
-		});		
-
+			url: 'http://content.guardianapis.com/search?page-size=1&format=json&callback=?',
+			success: function (json) {
+	
+				//	If we have a response and an array or results with just one item in it
+				//	then we have gotten back a valid result we may want to work with
+				if ('response' in json && 'results' in json.response && json.response.results.length == 1) {
+					control.parse(json.response.results[0]);
+				} else {
+					//	Something odd happened but I'm not quite sure what, possibly an over rate message
+					//	we'll do something super clever with it here
+				}
+			},
+			error: function () {
+				alert('need to catch this error');
+			}
+		});
+	
 	},
 
 	parse: function(json) {
@@ -43,12 +47,40 @@ control = {
 		//	Make sure it has an apiUrl and that it's different
 		//	(and therefor in theory, newer) that the current
 		if ('apiUrl' in json && json.apiUrl != control.latestAPIUrl && 'webTitle' in json) {
-			var h1 = $('<h1>').html(json.webTitle);
-            $('#container').append(h1);
+			
+			$('#container').fadeOut('slow', function() {
+				//	build the main headline
+				var h1 = $('<h1>').append($('<a>').attr('href', json.webUrl).html(json.webTitle)).addClass('section_' + json.sectionId);
+				//	build the section part
+				var h2 = $('<h2>').html('A moment ago in ').append($('<a>').attr('href','http://www.guardian.co.uk/' + json.sectionId).html(json.sectionName).addClass('section_' + json.sectionId));
+				
+				// put both of them into the container
+				$('#container').empty();
+				$('#container').append(h1);
+				$('#container').append(h2);
+				
+				//	Store the apiUrl so we know what to do next time
+				control.latestAPIUrl = json.apiUrl;
+				control.lastUpdate = parseInt(new Date().getTime()/1000, 10);
+				$('#container').fadeIn('fast');
+			});
+			
+		} else {
+			//	Otherwise let us just update the time
+			var newMins = Math.round((parseInt(new Date().getTime()/1000, 10) - control.lastUpdate)/60);
+			var tempHolder = $('#container h2 a').remove();
+			if (newMins === 0) {
+				$('#container h2').html('A moment ago in ').append(tempHolder);
+			} else if (newMins == 1) {
+				$('#container h2').html('About a minute ago in ').append(tempHolder);
+			} else {
+				$('#container h2').html('About ' + newMins + ' minutes ago in ').append(tempHolder);
+			}
+			
 		}
 		
 	}
 
 
 
-}
+};
